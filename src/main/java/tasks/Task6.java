de20b0ab4da51e676671f82faa6b4d6ddfda2d1d
Nikita Garaishin <kenzanter@gmail.com>
2024-12-14 22:25:32 +0700

    Добавлено решение задачи-6

--- src/main/java/tasks/Task6.java
+++ src/main/java/tasks/Task6.java
@@ -3,9 +3,9 @@ package tasks;
 import common.Area;
 import common.Person;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
 Имеются
@@ -19,6 +19,13 @@ public class Task6 {
   public static Set<String> getPersonDescriptions(Collection<Person> persons,
                                                   Map<Integer, Set<Integer>> personAreaIds,
                                                   Collection<Area> areas) {
-    return new HashSet<>();
+    Map<Integer, Area> areaMap = areas.stream()
+        .collect(Collectors.toMap(Area::getId, area -> area));
+
+    return persons.stream()
+        .flatMap(person -> personAreaIds.get(person.id()).stream()
+            .map(regionId -> person.firstName() + " - " + areaMap.get(regionId).getName())
+        )
+        .collect(Collectors.toSet());
   }
 }
