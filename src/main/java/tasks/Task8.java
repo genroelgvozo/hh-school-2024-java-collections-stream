f85dc5935f18d01fad5f59da25d39ee45d9e5338
aboger <bogerandr1@mail.ru>
2024-12-14 12:00:03 +0300

    Сделаны задачи 7 и 8

--- src/main/java/tasks/Task8.java
+++ src/main/java/tasks/Task8.java
@@ -4,14 +4,27 @@ import common.Person;
 import common.PersonService;
 import common.PersonWithResumes;
 import common.Resume;
-import java.util.Collection;
-import java.util.Set;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
   Еще один вариант задачи обогащения
-  На вход имеем коллекцию персон
+  На входе имеем коллекцию персон
   Сервис умеет по personId искать их резюме (у каждой персоны может быть несколько резюме)
   На выходе хотим получить объекты с персоной и ее списком резюме
+ */
+/* Не понятно, как должен работать / работает сервис PersonService, а конкретно, его методы.
+   Потому что я ожидаю, что буду передавать в findResumes() personId, а не Collection<Integer>.
+   Если для каждого personId существует ровно один Person, то можно предположить, что findPersons() вернет
+   набор Set<Person> из ровно того же числа элементов, что и на входе. Но при этом не понятно, чего ожидать
+   от findResumes(), если одному personId может соответствовать несколько резюме.
+
+   Так же не понятно, почему не сработала 3-я лямбда для Collectors.toMap:
+                (existing, replacement) -> {
+                existing.addAll(replacement);
+                return existing;}));
  */
 public class Task8 {
   private final PersonService personService;
@@ -21,7 +34,20 @@ public class Task8 {
   }
 
   public Set<PersonWithResumes> enrichPersonsWithResumes(Collection<Person> persons) {
-    Set<Resume> resumes = personService.findResumes(Set.of());
-    return Set.of();
+    Set<Resume> resumes = personService.findResumes(persons.stream().map(person -> person.id()).collect(Collectors.toSet()));
+    Map<Integer, Set<Resume>> resumesMap;
+
+    // Составляем HashMap из resumes. Так как они Set и каждый раз бегать по всему Set будет накладно.
+    resumesMap = resumes.stream().collect(Collectors.toMap(
+            resume -> resume.personId(),
+            resume -> Set.of(resume),
+            (existing, recent) -> Stream.concat(existing.stream(), recent.stream())
+                    .collect(Collectors.toSet())));
+
+      return persons.stream()
+            .map(person -> new PersonWithResumes(
+                    person,
+                    resumesMap.getOrDefault(person.id(), Set.of()))) // устранавливаю значение по умолчанию, чтобы возвращал пустой Set, а не Null
+            .collect(Collectors.toSet());
   }
 }
