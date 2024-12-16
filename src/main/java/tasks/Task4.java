097db4e49fbcff6ec8401dd35ef078642d41a93d
bazhenov-denis <denis.bazhenov2005@yandex.ru>
2024-12-16 19:18:35 +0300

    Task 4

--- src/main/java/tasks/Task4.java
+++ src/main/java/tasks/Task4.java
@@ -22,6 +22,10 @@ public class Task4 {
   }
 
   public List<ApiPersonDto> convert(List<Person> persons) {
-    return new ArrayList<>();
+    List<ApiPersonDto> personDto = new ArrayList<>();
+    for (Person person : persons) {
+      personDto.add(personConverter.convert(person));
+    }
+    return personDto;
   }
 }
