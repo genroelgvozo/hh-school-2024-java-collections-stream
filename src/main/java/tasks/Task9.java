0fd2a58c2789b2ece6e3da78cb8a1206500fde46
Anton <79048581346@mail.ru>
2024-12-17 18:53:59 +0300

    Edit Task9

--- src/main/java/tasks/Task9.java
+++ src/main/java/tasks/Task9.java
@@ -21,7 +21,8 @@ P.P.S Здесь ваши правки необходимо прокоммент
  */
 public class Task9 {
 
-  private long count;
+//  private long count;
+//  P.S убрана переменная count, больше негде использовать
 
   // Костыль, эластик всегда выдает в топе "фальшивую персону".
   // Конвертируем начиная со второй
@@ -35,24 +36,34 @@ public class Task9 {
 
   // Зачем-то нужны различные имена этих же персон (без учета фальшивой разумеется)
   public Set<String> getDifferentNames(List<Person> persons) {
-    return getNames(persons).stream().distinct().collect(Collectors.toSet());
+//    return getNames(persons).stream().distinct().collect(Collectors.toSet());
+//    P.S. .distinct не нужен, потому что toSet всё равно сделает множество
+//    P.S.S кажется конструкция слишком громоздкая, возможно переписать
+    return new HashSet<>(getNames(persons));
   }
 
   // Тут фронтовая логика, делаем за них работу - склеиваем ФИО
+  // P.S. переписано на StringBuilder, потому что при многократном использовании мы можем выиграть в скорости работы
   public String convertPersonToString(Person person) {
-    String result = "";
+    StringBuilder result = new StringBuilder();
     if (person.secondName() != null) {
-      result += person.secondName();
+      result.append(person.secondName());
     }
 
     if (person.firstName() != null) {
-      result += " " + person.firstName();
+      if (!result.isEmpty()) {
+        result.append(" ");
+      }
+      result.append(person.firstName());
     }
 
     if (person.secondName() != null) {
-      result += " " + person.secondName();
+      if (!result.isEmpty()) {
+        result.append(" ");
+      }
+      result.append(person.secondName());
     }
-    return result;
+    return result.toString();
   }
 
   // словарь id персоны -> ее имя
@@ -67,12 +78,14 @@ public class Task9 {
   }
 
   // есть ли совпадающие в двух коллекциях персоны?
+//  P.S. добавлен возврат значения has если нашли совпадения
   public boolean hasSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
     boolean has = false;
     for (Person person1 : persons1) {
       for (Person person2 : persons2) {
         if (person1.equals(person2)) {
           has = true;
+          return has;
         }
       }
     }
@@ -80,15 +93,17 @@ public class Task9 {
   }
 
   // Посчитать число четных чисел
+  // P.S. Убрана переменная count, по сути можно не вводить лишнюю переменную и сразу возвращать количество
   public long countEven(Stream<Integer> numbers) {
-    count = 0;
-    numbers.filter(num -> num % 2 == 0).forEach(num -> count++);
-    return count;
+    return numbers.filter(num -> num % 2 == 0).count();
   }
 
   // Загадка - объясните почему assert тут всегда верен
   // Пояснение в чем соль - мы перетасовали числа, обернули в HashSet, а toString() у него вернул их в сортированном порядке
-  void listVsSet() {
+//  P.S. содержание set и integers одинаковое, порядок один и тот же,
+  // если вызвать .equals у snapshot и set, то будет False (у них разный тип), но
+  // т.к. мы приводим их к строке, то set становится похожим на snapshot по содержимому и типу
+  static void listVsSet() {
     List<Integer> integers = IntStream.rangeClosed(1, 10000).boxed().collect(Collectors.toList());
     List<Integer> snapshot = new ArrayList<>(integers);
     Collections.shuffle(integers);
