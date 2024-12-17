756638bab4f7e6506f72dfa89ff6c290cb6ab225
Anton <79048581346@mail.ru>
2024-12-17 15:09:48 +0300

    Complete Task4

--- src/main/java/tasks/Task4.java
+++ src/main/java/tasks/Task4.java
@@ -22,6 +22,9 @@ public class Task4 {
   }
 
   public List<ApiPersonDto> convert(List<Person> persons) {
-    return new ArrayList<>();
+    List<ApiPersonDto> converteredPersons = persons.stream()
+        .map(personConverter::convert)
+        .toList();
+    return converteredPersons;
   }
 }
